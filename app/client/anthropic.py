cff93ddd263c69ff6e0bb30975f363944d8a5b7e
amanmulani09 <mulaniaman0504@gmail.com>
2026-05-07 23:52:11 +0530

    feat: create a Anthropic client and tools for llm calling

---
+++ app/client/anthropic.py
@@ -0,0 +1,84 @@
+from app.tools.tools import search_papers, extract_info
+from app.client.llmtools import tools
+import anthropic
+import json
+
+class AnthropicClient:
+    
+    def __init__(self) -> None:
+        self.client = anthropic.Anthropic(api_key="")
+        self.mapping_tool_function = {
+            "search_papers": search_papers,
+            "extract_info": extract_info
+        }
+        
+    def execute_tool(self,tool_name, tool_args):
+    
+        result = self.mapping_tool_function[tool_name](**tool_args)
+
+        if result is None:
+            result = "The operation completed but didn't return any results."
+            
+        elif isinstance(result, list):
+            result = ', '.join(result)
+            
+        elif isinstance(result, dict):
+            # Convert dictionaries to formatted JSON strings
+            result = json.dumps(result, indent=2)
+        
+        else:
+            # For any other type, convert using str()
+            result = str(result)
+        return result
+    
+    def process_query(query):
+        messages = [{'role': 'user', 'content': query}]
+        
+        response = self.client.messages.create(max_tokens = 2024,
+                                    #model = 'claude-3-7-sonnet-20250219', #deprecated model
+                                    model = 'claude-sonnet-4-6', 
+                                    tools = tools,
+                                    messages = messages)
+        
+        process_query = True
+        while process_query:
+            assistant_content = []
+
+            for content in response.content:
+                if content.type == 'text':
+                    
+                    print(content.text)
+                    assistant_content.append(content)
+                    
+                    if len(response.content) == 1:
+                        process_query = False
+                
+                elif content.type == 'tool_use':
+                    
+                    assistant_content.append(content)
+                    messages.append({'role': 'assistant', 'content': assistant_content})
+                    
+                    tool_id = content.id
+                    tool_args = content.input
+                    tool_name = content.name
+                    print(f"Calling tool {tool_name} with args {tool_args}")
+                    
+                    result = execute_tool(tool_name, tool_args)
+                    messages.append({"role": "user", 
+                                    "content": [
+                                        {
+                                            "type": "tool_result",
+                                            "tool_use_id": tool_id,
+                                            "content": result
+                                        }
+                                    ]
+                                    })
+                    response = client.messages.create(max_tokens = 2024,
+                                    #model = 'claude-3-7-sonnet-20250219', #deprecated model
+                                    model = 'claude-sonnet-4-6', 
+                                    tools = tools,
+                                    messages = messages) 
+                    
+                    if len(response.content) == 1 and response.content[0].type == "text":
+                        print(response.content[0].text)
+                        process_query = False
--- app/tools/tools.py
+++ app/tools/tools.py
@@ -61,3 +61,32 @@ def search_papers(topic:str, max_results: int = 5) -> List[str]:
     
     print(f"results are saved in : {file_path}")
     return paper_ids
+
+def extract_info(paper_id:str) -> str :
+    """
+    Search for information about a specific paper across all directories. 
+    
+    Args:
+        paper_id: the ID on the paper to look for 
+        
+    Returns: 
+        JSON string with paper information found, error message if not found 
+        
+    """
+    
+    for item in os.listdir(PAPER_DIR):
+        item_path = os.path.join(PAPER_DIR, item)
+        if os.path.isdir(item_path):
+            file_path = os.path.join(item_path, "papers_info.json")
+            if os.path.isfile(file_path):
+                try:
+                    with open(file_path, "r") as json_file:
+                        papers_info = json.load(json_file)
+                        if paper_id in papers_info:
+                            return json.dumps(papers_info[paper_id], indent=2)
+                except (FileNotFoundError, json.JSONDecodeError) as e:
+                    print(f"Error reading {file_path}: {str(e)}")
+                    continue
+    
+    return f"There's no saved information related to paper {paper_id}."
+    
